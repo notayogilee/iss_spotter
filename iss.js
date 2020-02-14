15efc31ec42f71bd0f0927484ed8feb29baf810c
Lee Castelani <notayogilee@gmail.com>
2020-02-13 20:44:42 -0500

    b

--- iss.js
+++ iss.js
@@ -18,4 +18,27 @@ const fetchMyIP = function(callback) {
 
   });
 };
-module.exports = { fetchMyIP };
+
+const fetchCoordsByIP = function(ip, callback) {
+  request(`https://ipvigilante.com/${ip}`, (error, response, data) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status code ${response.statusCode} when fetching coordinates for IP. Response: ${data}`;
+      callback(Error(msg), null);
+      return;
+    }
+    data = JSON.parse(data);
+    const latitude = `${data.data.latitude}`;
+    const longitude = `${data.data.longitude}`;
+
+    data = { latitude: latitude, longitude: longitude };
+
+    console.log(data);
+
+    callback(null, data);
+  });
+};
+module.exports = { fetchMyIP, fetchCoordsByIP };
